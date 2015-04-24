8fc55272286b99ff8c06ee5dc0fa31fcd0f0eaa9
kichithewolf <kichithewolf@gmail.com>
2015-04-24 10:37:18 -0400

    Additions to decide action

--- HandleInput.cpp
+++ HandleInput.cpp
@@ -19,7 +19,13 @@ Takes the given input and deals with it.
 */
 void HandleInput::DecideAction(string input, CharacterCreation character, SaveLoad savegame, MakeChoices choices) {
      if(input.compare("up") == 0) {
-        cout << "User entered up.\n";
+            cout << "User entered up.\n";
+     } else if(input.compare("down") == 0) {
+            cout << "user entered down.\n";
+     } else if(input.compare("left") == 0) {
+            cout << "user entered left\n";
+     } else if(input.compare("right") == 0) {
+            cout << "user entered right\n";
      } else if(input.compare("save") == 0) {
             cout << "Saving...\n";
             savegame.SaveGame(character);
@@ -33,8 +39,9 @@ void HandleInput::DecideAction(string input, CharacterCreation character, SaveLo
      } else if(input.compare("stats") == 0) {
             character.ShowAllStats();
      } else if(input.compare("exit") == 0) {
-            cout << "typed exit\n";
-     } else{
+            cout << "Exiting. Goodbye.\n";
+            exit(0);
+     } else {
             cout << "Input not valid!\n";
      }
 }
